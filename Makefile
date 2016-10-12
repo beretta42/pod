00c15d806924ff70ca9e7cbf2570a04c26daf1f9
Brett Gordon <beretta42@gmail.com>
2016-10-11 22:31:36 -0400

    working at low speed /w no jitter !!

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ pod.dsk: pod.bin AUTOEXEC.BAS
 	decb copy -a0l AUTOEXEC.BAS pod.dsk,AUTOEXEC.BAS
 
 pod.bin: pod.asm
-	lwasm -mpod.map -b -opod.bin pod.asm
+	lwasm -lpod.lst -mpod.map -b -opod.bin pod.asm
 
 clean:
 	rm -f pod.dsk pod.bin
--- pod.asm
+++ pod.asm
@@ -2,77 +2,6 @@
 ;;;  Podcast Player for CoCo
 ;;;
 
-
-
-	org	$e00
-
-frame	.dw	0		; entry frame pointer to exiting to BASIC
-irqs	.dw	0		; saved BASIC firq
-sndptr	.dw	$2000
-DCSTAT	.db	0		; read return status
-DCBPT	.dw	$2000		; drive buffer pointer
-lsnh	.db	0		; lsn no
-lsnm	.db	0
-lsnl	.db	0
-
-	
-start
-	orcc	#$50		; shut off interrupts (just in case)
-	sta	$ffd9		; high speed <-- FIXME
-	sts	frame		; save BASIC frame ptr
-	ldx	$10d		; save BASIC's firq ptr
-	stx	irqs		;
-	ldx	#noplay		; install out do nothing handler
-	stx	$10d		;
-	lda	$ff01		; enable pia's irq handling for hsync
-	ora	#$01		;
-	sta	$ff01		;
-	lda	$ff03		; disable pi's irq handling for vsync
-	anda	#~$01		;
-	sta	$ff03
-	lda	$ff23		; enable sound
-	ora	#$8
-	sta	$ff23
-	;; put sound in buffer
-a@	ldd	#$2000
-	std	DCBPT
-	lda	#$20
-	pshs	a
-b@	ldb	lsnh
-	ldx	lsnm
-	jsr	setup
-	jsr	execmd
-	inc	DCBPT
-	inc	lsnl
-	bne	c@
-	inc	lsnm
-	bne	c@
-	inc	lsnh
-c@	dec	,s
-	bne	b@
-	leas	1,s
-	andcc	#~$50		; turn on interrupts
-d@	lda	sndptr
-	cmpa	#$38		; at half way mark?
-	blo	d@
-	bra	a@		; loop
-
-
-;;; This firq handler does nothing
-noplay
-	tst	$ff00		; clear pia
-	ldx	sndptr		; get next DAC byte and 
-	lda	,x+		; 
-	sta	$ff20		; send it to pia
-	cmpx	#$4000		; wrap buffer?
-	bne	out@		; nope
-	;; wrap buffer
-	ldx	#$2000
-out@	stx	sndptr
-	rti			; return
-
-
-
 CTRLATCH    equ    $FF40          ; controller latch (write)
 CMDREG      equ    $FF48          ; command register (write)
 STATREG     equ    $FF48          ; status register (read)
@@ -82,61 +11,102 @@ PREG3       equ    $FF4B          ; param register 3
 DATREGA     equ    PREG2          ; first data register
 DATREGB     equ    PREG3          ; second data register
 
+
+BUFZ	equ	240
+ITER	equ	BUFZ/4
+WAIT	equ	BUFZ-ITER-1
+NORM	equ	(256-BUFZ)/2
+
+buf	equ	$2000	
+
+	org	$e00
+
+frame	.dw	0		; entry frame pointer to exiting to BASIC
+lsnh	.db	0		; lsn no
+lsnm	.db	0
+lsnl	.db	0
 	
-* SETUP - Setup the Command Packet
-* Setup VCMD with command, LSN and option byte
-* Entry: B = Bits 23-16 of LSN
-*        X = Bits 15-0  of LSN  
-setup
-	clr	DCSTAT		; clear error
-        ldy     #PREG2          ; set Y to point at Data Register A
+start
+	orcc	#$50		; shut off interrupts (just in case)
+	sta	$ffd8		; force low speed
+	sts	frame		; save BASIC frame ptr
+	lda	$ff01		; enable pia's irq handling for hsync
+	ora	#$01		;
+	sta	$ff01		;
+	lda	$ff03		; disable pi's irq handling for vsync
+	anda	#~$01		;
+	sta	$ff03
+	lda	$ff23		; enable sound
+	ora	#$8
+	sta	$ff23
+	ldy     #PREG2          ; set Y to point at Data Register A
+        ldb     #$43            ; write $43 to the controller..s/b 0x0B for Dragon (sixxie)
+	stb     CTRLATCH        ; ..latch to select Enhanced Mode
+	ldu	#buf
+	;; read issue sector read
+b@	ldb	lsnh
+	ldx	lsnm
+	;; start of read sector
         stb     -1,y            ; store LSN in the three..
         stx     ,y              ; ..Block Address registers
-        ldb     #$43            ; write $43 to the controller..s/b 0x0B for Dragon (sixxie)
-        stb     CTRLATCH        ; ..latch to select Enhanced Mode
-        ;; a wait should happen here, but the vectored routines and normal
-        ;; subroutining should give enough time already for the SDC to
-        ;; accept the control code.
-        rts
-
-* Entry: A = DW opcode
-execmd
-        ldy     #PREG2          ; set Y to point at data reg a
         lda     #$80            ; setup Read Sector command for target device
         sta     -2,y            ; send to command register (FF48)
-        exg     a,a             ; some time to digest the command
-        *** Wait for Controller Ready or Failed.
-        ldx     #0              ; long timeout counter = 65536
+	;; wait for 17 sync (hopefull enough time to finish seek)
+	ldb	#WAIT
+c@	sync
+	lda	,u+
+	ora	#$02
+	sta	$ff20
+	tst	$ff00
+	decb
+	bne	c@
+	;; Wait for Controller Ready or Failed. ;
 rdWait  lda     -2,y            ; read controller status
-*       bmi     rdFail          ; branch if FAILED bit is set
-        bita    #0x9c           ; any set bit is failure (autodetect)
-        bne     rdFail          ; yes then fail
         bita    #2              ; test the READY bit and..
-        bne     rdRdy           ; ..branch if ready
-        leax    -1,x            ; decrement timeout counter
-        bne     rdWait          ; continue polling until timeout
-rdFail  clr     CTRLATCH        ; return controller to emulation mode
-        ldb     #$80
-        stb     DCSTAT         ; set carry for failure
-        rts                     ; restore registers and return
-        *** Controller Ready. Read the Sector Data. Uses partial loop unrolling.
-rdRdy   ldx     DCBPT           ; move buffer ptr from U to X
-        ldd     #32*256+8       ; A = chunk count (32), B = bytes per chunk (8)
-rdChnk  ldu     ,y              ; read 1st pair of bytes for the chunk
-        stu     ,x              ; store to buffer
-        ldu     ,y              ; bytes 3 and 4 of..
-        stu     2,x             ; ..the chunk
-        ldu     ,y              ; bytes 5 and 6 of..
-        stu     4,x             ; ..the chunk
-        ldu     ,y              ; bytes 7 and 8 of..
-        stu     6,x             ; ..the chunk
-        abx                     ; increment X by chunk size (8)
-        deca                    ; decrement loop counter
+        beq     rdWait          ; ..branch if ready
+	ldx     #NORM           ; move buffer ptr from U to X
+rdChnk	ldd	,y
+	ora	#$02		; rs232 data out in mark
+	sync
+	sta	$ff20	
+	tst	$ff00
+	orb	#$02
+	sync
+	stb	$ff20
+	tst	$ff00
+	leax	-1,x
         bne     rdChnk          ; loop if more chunks to read
-        *** Return Success.
-        clr     CTRLATCH        ; clear carry / return controller to emulation mode
-        rts                     ; restore registers and return
-
+	;; get 32 bytes fast
+	lda	#ITER
+	pshs	a
+	ldx	#buf		; X will be our writer
+	tfr	x,u		; U will be our reader
+e@	ldd	,y
+	std	,x++
+	ldd	,y
+	std	,x++
+	;; wait for next sync
+	sync
+	lda	,u+
+	ora	#$02
+	sta	$ff20
+	tst	$ff00
+	dec	,s
+	bne	e@
+	leas	1,s
+	;; increment lsn to get ready for next sector read
+	inc	lsnl
+	bne	d@
+	inc	lsnm
+	bne	d@
+	inc	lsnh
+	;; wait for next sync
+d@	sync
+	lda	,u+
+	ora	#$02
+	sta	$ff20
+	tst	$ff00
+	jmp	b@		; loop
 
 
 	end	start
